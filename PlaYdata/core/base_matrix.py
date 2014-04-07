aac40563d26aeed1b62273499fd7fb9bcb3873fe
c3h3 <c3h3.tw@gmail.com>
2014-04-08 02:47:53 +0800

    [New Features] Matrix.building_values_index and Matrix.build_row_struct_index / [DEV Note] update daily dev note on 20140407

--- PlaYdata/core/base_matrix.py
+++ PlaYdata/core/base_matrix.py
@@ -14,12 +14,12 @@ class Matrix(np.ndarray):
     @classmethod
     def _preprocess_if_data_is_cls(cls, matrix, *args, **kwargs):
         # print "cls = ",cls
-        # print "matrix = ",matrix
-        # print "type(matrix) = ",type(matrix)
+        # print "data = ",data
+        # print "type(data) = ",type(data)
         # print "args = ",args
         # print "kwargs = ",kwargs
         # eval_cls = kwargs.get("eval_cls",1234)
-        # object.__setattr__(matrix, "_eval_cls", eval_cls)
+        # object.__setattr__(data, "_eval_cls", eval_cls)
         pass
 
     @classmethod
@@ -29,57 +29,76 @@ class Matrix(np.ndarray):
     def __new__(cls, data, dtype=None, force2d="as_row", *args, **kwargs):
 
         assert force2d in ("as_row", "as_col")
-        if isinstance(data, cls):
-            values_array = data
-            cls._preprocess_if_data_is_cls(matrix=values_array,
-                                           *args, **kwargs)
-            return data
 
+        if isinstance(data, cls):
+            matrix_data = data
+            cls._preprocess_if_data_is_cls(matrix=matrix_data, *args, **kwargs)
+            return matrix_data
         else:
 
-            values_array = np.array(data, dtype=dtype)
+            matrix_data = np.array(data, dtype=dtype)
 
             if force2d == "as_row":
-                if len(values_array.shape) < 2:
-                    values_array = np.array([values_array])
+                if len(matrix_data.shape) < 2:
+                    matrix_data = np.array([matrix_data])
             elif force2d == "as_col":
-                if len(values_array.shape) < 2:
-                    values_array = np.array([values_array]).T
-            
-            if len(values_array.shape) > 2:
-                values_array = np_tools.clean_no_data_tensors(values_array)
-            
-            assert len(values_array.shape) == 2
-            
-            
-            values_array = values_array.view(cls)
-            cls._preprocess_before_init_return(matrix=values_array, *args, **kwargs)
-            
-            return values_array
-    
+                if len(matrix_data.shape) < 2:
+                    matrix_data = np.array([matrix_data]).T
+
+            if len(matrix_data.shape) > 2:
+                matrix_data = np_tools.clean_no_data_tensors(matrix_data)
+
+            assert len(matrix_data.shape) == 2
+
+            matrix_data = matrix_data.view(cls)
+            cls._preprocess_before_init_return(matrix=matrix_data,
+                                               *args, **kwargs)
+
+            return matrix_data
+
     @property
     def _is_1d(self):
         return len(np_tools.clean_no_data_tensors(self).shape) == 1
-    
+
     @property
     def _as_1d_array(self):
         assert self._is_1d
         return np_tools.clean_no_data_tensors(self)
-    
+
     @property
     def _nrow(self):
         return self.shape[0]
-    
+
     @property
     def _ncol(self):
         return self.shape[1]
-    
+
     def _1d_ngram(self, n):
         assert self._is_1d
         ngram_results = list(tools.ngram(self.flatten(), n))
         return type(self)(data=ngram_results)
-    
-    
+
+    def build_values_index(self):
+        u, inv = np.unique(self, return_inverse=True)
+        states_matrix = StatesMatrix(data=u, eval_cls=type(self))
+        assert states_matrix._is_1d
+        idx_matrix = IndexMatrix(data=inv).reshape(self.shape)
+        return states_matrix, idx_matrix
+
+    def build_row_struct_index(self):
+        temp_self = self.copy()
+        temp_self.dtype = np.dtype(zip(map(str, range(self._ncol)),
+                                       [self.dtype] * self._ncol))
+
+        u, i, inv = np.unique(temp_self, return_index=True,
+                              return_inverse=True)
+
+        idx_matrix = IndexMatrix(inv)
+
+        states_matrix = StatesMatrix(self[i, :], eval_cls=type(u),
+                                     is_row_struct=True)
+        return states_matrix, idx_matrix
+
 
 class ValuesMatrix(Matrix):
     pass
@@ -90,27 +109,42 @@ class IndexMatrix(Matrix):
 
 
 class StatesMatrix(Matrix):
-    
+
+    @classmethod
+    def _set_is_row_struct(cls, matrix, *args, **kwargs):
+        is_row_struct = kwargs.get("is_row_struct", False)
+        object.__setattr__(matrix, "_is_row_struct", is_row_struct)
+
     @classmethod
     def _set_eval_cls(cls, matrix, *args, **kwargs):
         eval_cls = kwargs.get("eval_cls", np.ndarray)
         object.__setattr__(matrix, "_eval_cls", eval_cls)
-    
-    
+
     @classmethod
-    def _preprocess_if_data_is_cls(cls, matrix , *args, **kwargs):
-        cls._set_eval_cls(matrix=matrix , *args, **kwargs)
-    
-    
+    def _preprocess_if_data_is_cls(cls, matrix, *args, **kwargs):
+        cls._set_eval_cls(matrix=matrix, *args, **kwargs)
+        cls._set_is_row_struct(matrix=matrix, *args, **kwargs)
+
     @classmethod
-    def _preprocess_before_init_return(cls, matrix , *args, **kwargs):
-        cls._set_eval_cls(matrix=matrix , *args, **kwargs)
-        
-    
-    def _1d_eval(self, idx_mat):
+    def _preprocess_before_init_return(cls, matrix, *args, **kwargs):
+        cls._set_eval_cls(matrix=matrix, *args, **kwargs)
+        cls._set_is_row_struct(matrix=matrix, *args, **kwargs)
+
+    def _eval_1d_self(self, idx_mat):
         return self._as_1d_array[idx_mat].view(self._eval_cls).copy()
 
-    
+    def _eval_1d_idx_mat(self, idx_mat):
+        return self[idx_mat._as_1d_array, :].view(self._eval_cls).copy()
+
+    def _eval(self, idx_mat):
+
+        assert isinstance(idx_mat, IndexMatrix)
+        assert self._is_1d or (idx_mat._is_1d and self._is_row_struct)
+
+        if idx_mat._is_1d and self._is_row_struct:
+            return self._eval_1d_idx_mat(idx_mat=idx_mat)
+        else:
+            return self._eval_1d_self(idx_mat=idx_mat)
 
 
 if __name__ == '__main__':
--- PlaYdata/tests/test_base_matrix.py
+++ PlaYdata/tests/test_base_matrix.py
@@ -6,9 +6,9 @@ Created on Apr 7, 2014
 '''
 
 import numpy as np
-import scipy as sp
+# import scipy as sp
 import pandas as pd
- 
+
 test_text_df = pd.DataFrame([u"今天天氣很好",
                              u"今天天氣很爛",
                              u"我恨它",
@@ -21,22 +21,61 @@ test_text_df = pd.DataFrame([u"今天天氣很好",
 
 test_text_df.columns = ["text"]
 test_text_df = test_text_df.reset_index()
-test_text_df["idx"] = map(lambda xx :u"%05d" % xx,test_text_df["index"])
+test_text_df["idx"] = map(lambda xx: u"%05d" % xx, test_text_df["index"])
 test_text_df["r1"] = np.random.randn(test_text_df.shape[0])
 test_text_df["r2"] = np.random.randn(test_text_df.shape[0])
 
 
-from PlaYdata.core.base_matrix import Matrix
-import PlaYdata.util.tools as tools 
+from PlaYdata.core.base_matrix import Matrix, ValuesMatrix
+import PlaYdata.util.tools as tools
 
 
 def test_Matrix():
-    row_mat = Matrix(list(tools.ngram(test_text_df["text"].values[0],[1])),force2d="as_row")
-    col_mat = Matrix(list(tools.ngram(test_text_df["text"].values[0],[1])),force2d="as_col")
-    assert row_mat.shape == (1,6)
-    assert col_mat.shape  == (6,1)
+    row_mat = Matrix(list(tools.ngram(test_text_df["text"].values[0], [1])),
+                     force2d="as_row")
+
+    col_mat = Matrix(list(tools.ngram(test_text_df["text"].values[0], [1])),
+                     force2d="as_col")
+
+    assert row_mat.shape == (1, 6)
+    assert col_mat.shape == (6, 1)
     assert row_mat._is_1d
     assert col_mat._is_1d
+
+
+def test_Matrix_building_values_index():
+    val_mat1_text = list(tools.ngram(test_text_df["text"].values[0], [1]))
+    val_mat2_text = list(tools.ngram(test_text_df["text"].values[1], [1]))
+    val_mat1 = ValuesMatrix(val_mat1_text, force2d="as_col")
+    val_mat2 = ValuesMatrix(val_mat2_text, force2d="as_col")
+    val_mat = ValuesMatrix(np.concatenate((val_mat1._1d_ngram(2),
+                                           val_mat2._1d_ngram(2)), axis=0))
+    states_matrix, idx_matrix = val_mat.build_values_index()
+    assert np.array_equal(states_matrix._eval(idx_matrix), val_mat)
+
+
+def test_Matrix_build_row_struct_index():
+    val_mat1_text = list(tools.ngram(test_text_df["text"].values[0], [1]))
+    val_mat2_text = list(tools.ngram(test_text_df["text"].values[1], [1]))
+    val_mat1 = ValuesMatrix(val_mat1_text, force2d="as_col")
+    val_mat2 = ValuesMatrix(val_mat2_text, force2d="as_col")
+    val_mat = ValuesMatrix(np.concatenate((val_mat1._1d_ngram(2), val_mat2._1d_ngram(2)), axis=0))
+    states_matrix, idx_matrix = val_mat.build_row_struct_index()
+    assert np.array_equal(states_matrix._eval(idx_matrix), val_mat)
+
+
+def test_Matrix_build_index():
+    val_mat1_text = list(tools.ngram(test_text_df["text"].values[0], [1]))
+    val_mat2_text = list(tools.ngram(test_text_df["text"].values[1], [1]))
+    val_mat1 = ValuesMatrix(val_mat1_text, force2d="as_col")
+    val_mat2 = ValuesMatrix(val_mat2_text, force2d="as_col")
+    val_mat = ValuesMatrix(np.concatenate((val_mat1._1d_ngram(2),
+                                           val_mat2._1d_ngram(2)), axis=0))
+    states_matrix, idx_matrix = val_mat.build_values_index()
+    assert np.array_equal(states_matrix._eval(idx_matrix), val_mat)
+    ss, ii = idx_matrix.build_row_struct_index()
+    assert np.array_equal(ss._eval(ii), idx_matrix)
+    assert np.array_equal(states_matrix._eval(ss._eval(ii)), val_mat)
 
 
 
