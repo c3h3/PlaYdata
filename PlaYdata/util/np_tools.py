971b12afe1562a68e4a9a396d92745e7d93206e1
c3h3 <c3h3.tw@gmail.com>
2014-04-11 17:54:57 +0800

    [DEBUG] solve the bug of the function clean_no_data_tensors

--- PlaYdata/util/np_tools.py
+++ PlaYdata/util/np_tools.py
@@ -4,11 +4,22 @@ Created on Apr 6, 2014
 @author: c3h3
 '''
 
+# Has bugs in case of
+# > clean_no_data_tensors(np.array([[["12345"]]])
+#
+# def clean_no_data_tensors(np_array):
+#    return np_array[map(lambda xx:0 if xx==1 else slice(None,None,None),np_array.shape)]
+
+from PlaYdata.util.args_tools import check_args
+
 
 def clean_no_data_tensors(np_array):
-    return np_array[map(lambda xx:0 if xx==1 else slice(None,None,None),np_array.shape)]
 
-
+    clean_idx = map(lambda xx: 0 if xx == 1 else slice(None, None, None),
+                    np_array.shape)
+    if check_args(lambda xx: xx == 1, *np_array.shape):
+        clean_idx[0] = slice(None, None, None)
+    return np_array[clean_idx]
 
 
 if __name__ == '__main__':
